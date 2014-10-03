46cdd02dd54237ee4e82e093138217aa632fbb4c
deep-泰飞 <373345619@qq.com>
2014-10-04 04:09:12 +0800

    add angularjs

---
+++ angularjs/js/app.js
@@ -0,0 +1,59 @@
+angular.module('demo', [])
+    .controller('navCtrl',function($scope,$location){
+        $scope.isActive = function (viewLocation) {
+            return viewLocation === $location.path();
+        };
+    })
+    .controller('tabsCtrl', ['$scope', function ($scope) {
+    	
+    }])
+    .controller('formCtrl', ['$scope', function ($scope) {
+    	
+    }])
+    .controller('loadingCtrl', ['$scope', function ($scope) {
+    	
+    }])
+	.config(function ($routeProvider) {
+		$routeProvider.when('/tabs', {
+			templateUrl: '../page/tabs.html',
+			controller: 'tabsCtrl',
+			title: 'tabs',
+			info: '一个简单的选项卡切换效果'
+		}).when('/form', {
+			templateUrl: '../page/form.html',
+			controller: 'formtCtrl',
+			title: 'form validate',
+			info: '基本表单验证代码'
+		}).when('/', {
+	        templateUrl: '../page/tabs.html',
+	        controller: 'tabsCtrl',
+	        title: 'Demo™',
+			info: '我的AngularJS学习笔记'
+	    }).when('/loading', {
+	        templateUrl: '../page/loading.html',
+	        controller: 'loadingCtrl',
+	        title: 'loading'
+	    })
+
+	})
+	.directive('loading', function () {
+      return {
+        restrict: 'E',
+        replace:true,
+        template: '<div class="loading"></div>',
+        link: function (scope, element, attr) {
+              scope.$watch('loading', function (val) {
+                  if (val)
+                      $(element).show();
+                  else
+                      $(element).hide();
+              });
+        }
+      }
+  })
+	.run(['$location', '$rootScope', function($location, $rootScope) {
+     $rootScope.$on("$routeChangeSuccess", function(event, currentRoute, previousRoute) {
+        $rootScope.title = currentRoute.title;
+         $rootScope.info = currentRoute.info;
+    });
+}])
